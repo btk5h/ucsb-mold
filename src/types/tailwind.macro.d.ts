347f6065c695339f5727422ca17dba1bbccf223d
Bryan Terce <bryan.terce@gmail.com>
2019-11-07 14:03:02 -0800

    Fix tailwind types

--- src/types/tailwind.macro.d.ts
+++ src/types/tailwind.macro.d.ts
@@ -1,8 +1,8 @@
 declare module "tailwind.macro" {
   import { CSSProperties, StyledInterface } from "styled-components"
 
-  type TailwindInterface = (s: TemplateStringsArray, o: any) => CSSProperties
-  declare const tw: StyledInterface & TailwindInterface
+  type TailwindInterface = (s: TemplateStringsArray, o?: any) => any
+  declare const tw: TailwindInterface & StyledInterface
 
   export = tw
 }
