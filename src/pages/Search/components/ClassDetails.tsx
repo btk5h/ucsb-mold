96af016cccf7c4b71358bfd3fbc74513057d4e1f
Bryan Terce <bryan.terce@gmail.com>
2019-11-12 10:27:52 -0800

    Display class units and grading options

--- src/pages/Search/components/ClassDetails.tsx
+++ src/pages/Search/components/ClassDetails.tsx
@@ -20,23 +20,53 @@ const Title = tw.h1`
   text-2xl
 `
 
+const Units = tw.span`
+  mr-4
+`
+
+const Description = tw.p`
+  mt-2
+`
+
+function gradingOptionToString(gradingOption?: string) {
+  switch (gradingOption) {
+    case "P":
+      return "Pass/No Pass Only"
+    case "L":
+      return "Letter Grade Only"
+  }
+
+  return "Optional Grading"
+}
+
+function unitsString(c: Class) {
+  if (c.unitsFixed) {
+    return `${c.unitsFixed} Unit${c.unitsFixed > 1 && "s"}`
+  }
+
+  if (c.unitsVariableHigh) {
+    return `${c.unitsVariableLow} - ${c.unitsVariableHigh} Units`
+  }
+}
+
 type ClassDetailsProps = {
   class: Class
 }
 
-const ClassDetails: React.FC<ClassDetailsProps> = (props) => {
-  const {
-    class: info
-  } = props
+const ClassDetails: React.FC<ClassDetailsProps> = props => {
+  const { class: info } = props
   return (
     <Wrapper>
-      <Title><CourseId>{info.courseId}</CourseId> {info.title}</Title>
-      <p>{info.description}</p>
-      {
-        info.classSections && info.classSections.map(s => (
+      <Title>
+        <CourseId>{info.courseId}</CourseId> {info.title}
+      </Title>
+      <Units>{unitsString(info)}</Units>
+      {gradingOptionToString(info.gradingOption)}
+      <Description>{info.description}</Description>
+      {info.classSections &&
+        info.classSections.map(s => (
           <SectionDetails key={s.section} section={s} />
-        ))
-      }
+        ))}
     </Wrapper>
   )
 }
