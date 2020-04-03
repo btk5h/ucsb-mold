071b01c230ebf31b77a38511ff356d405a2c0692
Bryan Terce <bryan.terce@gmail.com>
2020-04-03 01:13:41 -0700

    Add environment variable loading

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
+const dotEnvResult = require("dotenv").config();
+
+if (dotEnvResult.error) {
+  throw dotEnvResult.error;
+}
+
 module.exports = {
+  env: {
+    UCSB_API_KEY: process.env.UCSB_API_KEY,
+    UCSB_API_SECRET: process.env.UCSB_API_SECRET,
+  },
   experimental: {
     jsconfigPaths: true,
   },
