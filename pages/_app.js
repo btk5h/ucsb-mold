2233bb87f9a5ba882f2a4ee3a1825c6b06d7e33a
Bryan Terce <bryan.terce@gmail.com>
2020-04-03 00:08:18 -0700

    Add `tailwindcss` support via `twin.macro`

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import "tailwindcss/dist/base.css";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        "ucsb-navy": "#003660",
+        "ucsb-gold": "#febc11",
+        "off-black": "#3d4952",
+        highlight: "#eef0f2",
+        darken: "rgba(0, 0, 0, 0.5)",
+      },
+    },
+  },
+};
